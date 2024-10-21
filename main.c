f497c66ba87bb2177829a07e03f143d2213ccc63
JackHicks02 <jackhicks02@outlook.com>
2024-10-21 21:37:47 +0100

    feat(main): test or

--- main.c
+++ main.c
@@ -3,14 +3,28 @@
 int nand(int a, int b);
 int not(int a);
 int and (int a, int b);
+int or (int a, int b);
 
 int main(void)
 {
-    int a = 1, b = 1;
-
-    printf("NAND of %d and %d is %d\n", a, b, nand(a, b));
-    printf("NOT of %d and is %d\n", a, not(a));
-    printf("AND of %d and %d is %d\n", a, b, and(a, b));
+    test();
 
     return 0;
+}
+
+int test(void)
+{
+    for (int a = 0; a <= 1; a++)
+    {
+        for (int b = 0; b <= 1; b++)
+        {
+            printf("------------------------------\n");
+            printf("A = %d | B = %d \n", a, b);
+            printf("NAND of %d and %d is %d\n", a, b, nand(a, b));
+            printf("NOT of %d and is %d\n", a, not(a));
+            printf("AND of %d and %d is %d\n", a, b, and(a, b));
+            printf("OR of %d and %d is %d\n", a, b, or(a, b));
+            printf("------------------------------\n");
+        }
+    }
 }
