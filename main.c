b783877c3f88cced87f681add40ea6e65d31227e
JackHicks02 <jackhicks02@outlook.com>
2024-10-21 20:57:52 +0100

    feat(main): and test

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 
 int nand(int a, int b);
 int not(int a);
+int and (int a, int b);
 
 int main(void)
 {
     int a = 1, b = 1;
-    int result = nand(a, b);
 
-    printf("NAND of %d and %d is %d\n", a, b, result);
+    printf("NAND of %d and %d is %d\n", a, b, nand(a, b));
     printf("NOT of %d and is %d\n", a, not(a));
+    printf("AND of %d and %d is %d\n", a, b, and(a, b));
 
     return 0;
 }
