540446e8c97276a6ad61aeca566e35b49ec4bb5d
JackHicks02 <jackhicks02@outlook.com>
2024-10-23 23:46:32 +0100

    feat(gates.c): one file to contain all gates

---
+++ gates/gates.c
@@ -0,0 +1,45 @@
+int nand(int a, int b)
+{
+    return ~(a & b) & 1;
+    // mask with &1 to get least significant bit
+    // should probably just mask at outputs
+}
+
+int not(int a)
+{
+    return nand(a, a);
+}
+
+int and (int a, int b)
+{
+    return not(nand(a, b));
+}
+
+int or (int a, int b)
+{
+    // not(not(a) and not(b))
+    return nand(not(a), not(b));
+}
+
+int xor (int a, int b) 
+{
+    // a or b and not (a and b)
+    return (nand(nand(a, nand(a, b)), nand(b, nand(a, b))));
+}
+
+int mux(int a, int b, int sel)
+{
+    return (or (and(a, not(sel)), and(b, sel)));
+}
+
+int demux(int in, int sel)
+{
+    // DemuxResult result = {
+    //     out1: and(in, not(sel)),
+    //     out2: and(in, sel)
+    // };
+
+    int out1 = and(in, not(sel));
+    int out2 = and(in, sel);
+    return (out1 + (out2 << 1));
+}
