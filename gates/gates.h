55d1f338dd20e54809a523c50b049954c2677559
JackHicks02 <jackhicks02@outlook.com>
2024-10-23 23:58:34 +0100

    feat(gates.h): monolith gates header

---
+++ gates/gates.h
@@ -0,0 +1,19 @@
+#ifndef GATES_H
+#define GATES_H
+
+// some of the gates are c++ keywords and it makes it look disgusting :(
+int nand(int a, int b);
+
+int not(int a);
+
+int and (int a, int b);
+
+int or(int a, int b);
+
+int demux(int in, int sel);
+
+int mux(int a, int b, int s);
+
+int xor(int in, int sel);
+
+#endif
