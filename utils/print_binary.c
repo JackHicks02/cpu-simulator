46252a52d8dc846cb273ff59205e23361ff9f27a
Jack Hicks <jackhicks02@outlook.com>
2024-10-29 15:02:02 +0000

    style(print_binary): clangd

--- utils/print_binary.c
+++ utils/print_binary.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include "./get_bit.h"
+#include <stdio.h>
 
-void print_binary(int number, int bit)
-{
-    char out[bit + 1];
+void print_binary(int number, int bit) {
+  char out[bit + 1];
 
-    for (int i = 0; i < bit; i++)
-    {
-        out[bit - i - 1] = get_bit(number, i) ? '1' : '0';
-    }
-    out[bit] = '\0';
+  for (int i = 0; i < bit; i++) {
+    out[bit - i - 1] = get_bit(number, i) ? '1' : '0';
+  }
+  out[bit] = '\0';
 
-    printf("%s\n", out);
+  printf("%s\n", out);
 }
