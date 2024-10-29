2bc9011166e84ba2110c35a62f289b12e7dce3b7
Jack Hicks <jackhicks02@outlook.com>
2024-10-29 15:41:47 +0000

    refactor(main): prepend with gate_

--- main.c
+++ main.c
@@ -7,17 +7,17 @@ typedef signed char byte;
 
 void test(void) {
   printf("------------------------------\n");
-  printf("size of int %ldbytes | %ldbit\n", sizeof(int), sizeof(int) * 8);
+  printf("size of int %llubytes | %llubit\n", sizeof(int), sizeof(int) * 8);
   printf("------------------------------\n");
   for (int a = 0; a <= 1; a++) {
     for (int b = 0; b <= 1; b++) {
       printf("------------------------------\n");
       printf("A = %d | B = %d \n", a, b);
-      printf("NAND of %d and %d is %d\n", a, b, nand(a, b));
-      printf("NOT of %d and is %d\n", a, not(a));
-      printf("AND of %d and %d is %d\n", a, b, and(a, b));
-      printf("OR of %d and %d is %d\n", a, b, or (a, b));
-      printf("XOR of %d and %d is %d\n", a, b, xor(a, b));
+      printf("NAND of %d and %d is %d\n", a, b, gate_nand(a, b));
+      printf("NOT of %d and is %d\n", a, gate_not(a));
+      printf("AND of %d and %d is %d\n", a, b, gate_and(a, b));
+      printf("OR of %d and %d is %d\n", a, b, gate_or(a, b));
+      printf("XOR of %d and %d is %d\n", a, b, gate_xor(a, b));
       printf("------------------------------\n");
     }
   }
@@ -26,7 +26,8 @@ void test(void) {
   for (int a = 0; a <= 1; a++) {
     for (int b = 0; b <= 1; b++) {
       for (int s = 0; s <= 1; s++) {
-        printf("A = %d | B = %d | S = %d | MUX = %d\n", a, b, s, mux(a, b, s));
+        printf("A = %d | B = %d | S = %d | MUX = %d\n", a, b, s,
+               gate_mux(a, b, s));
       }
     }
   }
@@ -35,14 +36,14 @@ void test(void) {
   printf("---------DEMUX TEST-----------\n");
   for (int in = 0; in <= 1; in++) {
     for (int sel = 0; sel <= 1; sel++) {
-      int demuxResult = demux(in, sel);
+      int demuxResult = gate_demux(in, sel);
       printf("sel = %d | in = %d | out1 = %d | out2 = %d\n", sel, in,
              demuxResult & 1, (demuxResult >> 1) & 1);
     }
   }
   printf("--------END DEMUX TEST--------\n");
   printf("---------NOT 16-----------\n");
-  print_binary(not16(700001), 16);
+  print_binary(gate_not16(700001), 16);
   printf("--------END DEMUX TEST--------\n");
 }
 
