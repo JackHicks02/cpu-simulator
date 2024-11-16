882df606864135837f6d6ba98f05e4feb65b35a1
JackHicks02 <jackhicks02@outlook.com>
2024-11-16 18:39:28 +0000

    feat(main.c): persist sdl window

--- src/main.c
+++ src/main.c
@@ -1,29 +1,47 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // TODO: replace int with this, char is 1 byte...
 typedef signed char byte;
 
-int main(void) {
-  printf("\n");
+SDL_Window *initaliseWindow(void) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   SDL_Window *window =
-      SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+      SDL_CreateWindow("Screen", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                       800, 600, SDL_WINDOW_SHOWN);
 
   if (!window) {
     printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     SDL_Quit();
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
-  //gcc src/main.c -o main -lSDL2 
+  return window;
+}
 
-  SDL_Delay(3000); // Keep the window open for 3 seconds
+int main(void) {
+
+  SDL_Window *window = initaliseWindow();
+
+  // gcc src/main.c -o main -lSDL2
+  byte isRunning = 1;
+  SDL_Event ev;
+
+  while (isRunning) {
+    while (SDL_PollEvent(&ev) != 0) {
+      if (ev.type == SDL_QUIT)
+        isRunning = 0;
+    }
+
+    SDL_UpdateWindowSurface(window);
+  }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
