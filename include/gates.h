b10c4df5eb0cc09b00b8b699da20ea041f3d1f1e
JackHicks02 <jackhicks02@outlook.com>
2024-11-19 21:25:49 +0000

    feat(gates.h): unsigned 8bit and 16bit type

--- include/gates.h
+++ include/gates.h
@@ -1,8 +1,12 @@
 #ifndef GATES_H
 #define GATES_H
 
-// some of the gates are c++ keywords and it makes it look disgusting :(
-int gate_nand(int a, int b);
+#include <cstdint>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+int gate_nand(u8 a, int b);
 
 int gate_not(int a);
 
@@ -34,5 +38,6 @@ int gate_mux8way16(int a, int b, int c, int d, int e, int f, int g, int h,
 int gate_demux4way(int in, int sel);
 
 int gate_demux8way(int in, int sel);
+
 
 #endif
